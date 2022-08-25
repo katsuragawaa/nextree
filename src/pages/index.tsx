3c85f913d191cb8c32c76bbd68d1629894c2f074
katsuragawa <andre.katsuragawa@gmail.com>
2022-08-24 22:37:37 -0300

    add some ui placeholders

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,10 +10,25 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
-        <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-600">
+      <main className="container mx-auto flex min-h-screen flex-col items-center p-8">
+        <h1 className="m-8 text-5xl font-extrabold leading-normal text-gray-600 md:text-7xl">
           Nex<span className="text-teal-400">tree</span>
         </h1>
+
+        <div className="w-4/5 my-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Nunc mattis enim ut tellus elementum sagittis vitae et leo. Sit amet tellus cras adipiscing
+          enim.
+        </div>
+
+        <div className="flex h-96 my-4 w-4/5 gap-12">
+          <div className="flex-1 rounded-lg border-2 border-teal-400">
+            <div className="flex h-full items-center justify-center">left container</div>
+          </div>
+          <div className="flex-1 rounded-lg border-2 border-teal-400">
+            <div className="flex h-full items-center justify-center">right container</div>
+          </div>
+        </div>
       </main>
     </>
   );
